367d3b2044fa95a528685878f94a886b31031685
luciancic <luciancondrea95@gmail.com>
2018-06-23 11:00:43 -0400

    Add api route

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Coming soon!'));
+app.get('/api', (req, res) => res.send('API also coming soon!'));
+
 
 app.listen(PORT);
