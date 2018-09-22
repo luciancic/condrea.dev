566c5a58aea3abdb7df5220fd04b22c11b3eda43
luciancic <luciancondrea95@gmail.com>
2018-09-21 23:14:44 -0400

    Add Google Analytics

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Coming soon!'));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')));
 app.get('/api', (req, res) => res.send('API also coming soon!'));
 
 
