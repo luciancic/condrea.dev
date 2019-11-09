c86ce2225f9e49a1244b1fb7bf8544979ad7d33f
Lucian Condrea <luciancondreadev@gmail.com>
2019-11-09 18:38:58 -0500

    add intro section

---
+++ src/components/intro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <section>
+      <h1>Lucian Condrea</h1>
+      <span>Fullstack Web Developer</span>
+    </section>
+  )
+}
+
+export default Intro
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from '../components/header'
+import Intro from '../components/intro'
 import '../styles.scss'
 
 const IndexPage = () => (
   <div>
     <Header />
+    <Intro />
   </div>
 )
 
