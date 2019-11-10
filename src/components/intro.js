ee326317d1170bd8428c401daa7705355a860f2b
Iuliu Pop <iuliu.laurentiu.pop@gmail.com>
2019-11-09 19:35:23 -0500

    iuliu's work (#4)

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import EmailIcon from '../images/email-icon.svg'
+import GithubIcon from '../images/github-icon.svg'
 
 function Intro() {
   return (
-    <section>
-      <h1>Lucian Condrea</h1>
-      <span>Fullstack Web Developer</span>
-    </section>
+    <main>
+      <div>
+        <h1>Lucian Condrea</h1>
+        <span>Fullstack Web Developer</span>
+      </div>
+
+      <address>
+        <span>Contact Me:</span>
+        <a href="#"><img src={EmailIcon} alt="Email Icon Link"/></a>
+        <a href="#"><img src={GithubIcon} alt="Git Hub Icon Link"/></a>
+      </address>
+    </main>
   )
 }
 
