f757799c08cce360c6fd71cf15d01c5fead13981
Iuliu Pop <iuliu.laurentiu.pop@gmail.com>
2019-11-09 19:33:05 -0500

    iuliu's work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Header from '../components/header'
-import Intro from '../components/intro'
+import Header from '../components/Header'
+import Intro from '../components/Intro'
 import '../styles.scss'
 
 const IndexPage = () => (
---
+++ src/utils/typography.js
@@ -0,0 +1,11 @@
+import Typography from "typography"
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    "Lato",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Lato", "sans-serif"],
+})
+export default typography
