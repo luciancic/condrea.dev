9b83ac766e42a614a76d624695c46a26c58f9339
Lucian Condrea <luciancondreadev@gmail.com>
2020-02-23 20:34:03 -0500

    add social icons

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,24 +1,24 @@
 import React from "react"
-import EmailIcon from "../images/email-icon.svg"
-// import GithubIcon from '../images/github-icon.svg'
-
+import EmailIcon from "../images/email.svg"
+import GithubIcon from '../images/github.svg'
+import LinkedinIcon from '../images/linkedin.svg'
 function Action() {
   return (
     <nav>
       <ul>
         <li>
           <span>
-            <img src={EmailIcon} alt="email" />
+            <img src={LinkedinIcon} height="42" alt="email" />
           </span>
         </li>
         <li>
           <span>
-            <img src={EmailIcon} alt="email" />
+            <img src={GithubIcon} height="42" alt="email" />
           </span>
         </li>
         <li>
           <span>
-            <img src={EmailIcon} alt="email" />
+            <img src={EmailIcon} height="42" alt="email" />
           </span>
         </li>
       </ul>
